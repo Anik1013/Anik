2b473f2840e25edb7d8d746f5d1cd7425d167157
Md. Abdul Kader Anik <144541494+Anik1013@users.noreply.github.com>
2023-09-13 23:02:05 +0600

    Update like.js

--- like.js
+++ like.js
@@ -1,1 +1,34 @@
+// Get references to the button and like count elements
+const likeButton = document.getElementById('likeButton');
+const likeCount = document.getElementById('likeCount');
+
+// Initialize the like count
+let currentLikes = 0;
+
+// Function to update the like count display
+function updateLikeCount() {
+    likeCount.textContent = currentLikes;
+}
+
+// Function to handle the "like" button click event
+function toggleLike() {
+    if (likeButton.textContent === 'Like') {
+        // User clicked to like the content
+        currentLikes++;
+        likeButton.textContent = 'Unlike';
+    } else {
+        // User clicked to unlike the content
+        currentLikes--;
+        likeButton.textContent = 'Like';
+    }
+
+    // Update the like count display
+    updateLikeCount();
+}
+
+// Attach the toggleLike function to the button's click event
+likeButton.addEventListener('click', toggleLike);
+
+// Initial display of like count
+updateLikeCount();
 
